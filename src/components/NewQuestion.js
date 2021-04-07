ef5b87eeae3c72edfe91b383236af6948dd18edd
Krushil <krushilnaik96@gmail.com>
2021-04-06 20:17:42 -0400

    flesh out new question form

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -2,7 +2,21 @@ import React from 'react';
 import './scss/NewQuestion.scss';
 
 function NewQuestion() {
-	return <div>New Question</div>;
+	return (
+		<div className="new-question card">
+			<h1 className="card-header bg-white">Create New Question</h1>
+
+			<form>
+				<h6>Complete the question:</h6>
+				<h4>Would you rather...</h4>
+
+				<input type="text" name="option_one" id="option_one" placeholder='Enter Option One Text Here:' />
+				<div>OR</div>
+				<input type="text" name="option_two" id="option_two" placeholder='Enter Option Two Text Here:' />
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	);
 }
 
 export default NewQuestion;
