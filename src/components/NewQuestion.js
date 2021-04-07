0fdd8b6970cff1a740b462aee5a99191c53a50b9
Krushil <krushilnaik96@gmail.com>
2021-04-06 20:34:31 -0400

    style the new question form

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -4,16 +4,20 @@ import './scss/NewQuestion.scss';
 function NewQuestion() {
 	return (
 		<div className="new-question card">
-			<h1 className="card-header bg-white">Create New Question</h1>
+			<h2 className="card-header bg-white">Create New Question</h2>
 
 			<form>
 				<h6>Complete the question:</h6>
 				<h4>Would you rather...</h4>
 
-				<input type="text" name="option_one" id="option_one" placeholder='Enter Option One Text Here:' />
-				<div>OR</div>
-				<input type="text" name="option_two" id="option_two" placeholder='Enter Option Two Text Here:' />
-				<button type="submit">Submit</button>
+				<input className='form-control' type="text" name="option_one" id="option_one" placeholder='Enter Option One Text Here:' />
+				<div className="divider">
+					<hr />
+					<span className='or'>OR</span>
+					<hr />
+				</div>
+				<input className='form-control'  type="text" name="option_two" id="option_two" placeholder='Enter Option Two Text Here:' />
+				<button className='form-control' type="submit">Submit</button>
 			</form>
 		</div>
 	);
