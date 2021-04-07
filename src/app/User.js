f9ca82b9f32655064531ef96dddf08d72c2b8271
Krushil <krushilnaik96@gmail.com>
2021-04-07 09:51:48 -0400

    store user info in an object

---
+++ src/app/User.js
@@ -0,0 +1,30 @@
+class User {
+	/**
+	 * @param {string} name - user's name
+	 * @param {string} avatar - locationi of user's avatar
+	 */
+	constructor(name, avatar = 'assets/images/react-redux.jpeg') {
+		this.name = name;
+		this.avatar = avatar;
+
+		/**
+		 * @type { {questionID: number, answer: number}[] }
+		 */
+		this.questionsAnswered = [];
+
+		/**
+		 * @type {number[]}
+		 */
+		this.questionsAsked = [];
+	}
+
+	/**
+	 * @param {number} questionID - id of the question the user answered
+	 * @param {number} answer - user choice
+	 */
+	answerQuestion(questionID, answer) {
+		this.questionsAnswered.push( {questionID, answer} );
+	}
+}
+
+export default User;
