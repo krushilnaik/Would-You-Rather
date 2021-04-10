8705617695072dd16353da285bda1f3768d7ea1e
Krushil <krushilnaik96@gmail.com>
2021-04-10 15:24:49 -0400

    make English more good

--- src/components/App.js
+++ src/components/App.js
@@ -52,7 +52,7 @@ function App(props) {
 			<Switch>
 				<Route exact path='/' render={() => (user ? <Home /> : <SignInForm />)} />
 				<Route exact path='/add' render={() => (user ? <NewQuestion /> : <MustSignIn message='ask a question'/>)} />
-				<Route exact path='/leaderboard' render={() => (user ? <LeaderBoard /> : <MustSignIn  message='flex your inquire skills'/>)} />
+				<Route exact path='/leaderboard' render={() => (user ? <LeaderBoard /> : <MustSignIn  message='flex your inquiring skills'/>)} />
 				<Route path='/questions/:id' children={user ? <Question /> : <MustSignIn  message='answer questions' />} />
 				<Route path='*' render={() => <PageNotFound />} />
 			</Switch>
